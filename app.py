2e93da9978412588ad24b3f4784ca8a5a60b7e3b
Fernando Mendoza <158506427+FernandoM42@users.noreply.github.com>
2025-08-14 16:01:57 -0500

    .

--- app.py
+++ app.py
@@ -2509,4 +2509,5 @@ def ranking():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
